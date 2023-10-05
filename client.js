9b6ec56a905ceaab0fa5c04615f65c43f3238b91
thwuong <duongthuong654@gmail.com>
2023-10-05 16:31:40 +0700

    first

---
+++ client.js
@@ -0,0 +1,47 @@
+// const publicVapidKey = "BOynOrGhgkj8Bfk4hsFENAQYbnqqLSigUUkCNaBsAmNuH6U9EWywR1JIdxBVQOPDbIuTaj0tVAQbczNLkC5zftw";
+function urlBase64ToUint8Array(base64String) {
+    var padding = "=".repeat((4 - (base64String.length % 4)) % 4);
+    var base64 = (base64String + padding).replace(/\-/g, "+").replace(/_/g, "/");
+
+    var rawData = window.atob(base64);
+
+    var outputArray = new Uint8Array(rawData.length);
+
+    for (var i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+}
+if ("serviceWorker" in navigator) {
+    console.log("Registering service worker");
+
+    run().catch((error) => console.error(error));
+}
+
+async function run() {
+    console.log("Registering service worker");
+    const registration = await navigator.serviceWorker.register("/worker.js", { scope: "/" });
+    console.log("Registered service worker");
+
+    const response = await fetch("./register");
+    const publicVapidKey = await response.text();
+
+    console.log("Registering push");
+    const subscription = await registration.pushManager.subscribe({
+        userVisibleOnly: true,
+        // The `urlBase64ToUint8Array()` function is the same as in
+        // https://www.npmjs.com/package/web-push#using-vapid-key-for-applicationserverkey
+        applicationServerKey: urlBase64ToUint8Array(publicVapidKey),
+    });
+    console.log("Registered push");
+
+    console.log("Sending push");
+    await fetch("/subscribe", {
+        method: "POST",
+        body: JSON.stringify(subscription),
+        headers: {
+            "content-type": "application/json",
+        },
+    });
+    console.log("Sent push");
+}
---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const webPush = require("web-push");
+const app = express();
+
+const vapidKeys = webPush.generateVAPIDKeys();
+
+webPush.setVapidDetails("mailto:duongthuong654@gmail.com", vapidKeys.publicKey, vapidKeys.privateKey);
+
+app.use(express.json());
+app.use(require("body-parser").json());
+
+app.post("/subscribe", (req, res) => {
+    const subScription = req.body;
+    res.status(201).json({});
+    const payload = JSON.stringify({ title: "test" });
+
+    console.log(subScription);
+
+    webPush.sendNotification(subScription, payload).catch((error) => {
+        console.error(error.stack);
+    });
+});
+
+app.get("/register", (req, res) => {
+    res.send(vapidKeys.publicKey);
+});
+app.use(require("express-static")("./"));
+
+app.listen(3000, (req, res) => {
+    console.log("http://localhost:3000");
+});
