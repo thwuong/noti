2961d0c5717c45ae99a52de9c5ae14f1e158f68d
thwuong <duongthuong654@gmail.com>
2023-10-05 17:37:14 +0700

    fix

--- client.js
+++ client.js
@@ -21,7 +21,6 @@ async function run() {
     console.log("Registering service worker");
     const registration = await navigator.serviceWorker.register("/worker.js", { scope: "/" });
     console.log("Registered service worker");
-
     const response = await fetch("./register");
     const publicVapidKey = await response.text();
 
